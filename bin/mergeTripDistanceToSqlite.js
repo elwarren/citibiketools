82db1750f3aff3b16be70353542bc4f48482edcb
Warren Lindsey <warren.lindsey@gmail.com>
2014-08-05 00:27:33 -0400

    trip estimates work great, no quota throttling yet

--- bin/mergeTripDistanceToSqlite.js
+++ bin/mergeTripDistanceToSqlite.js
@@ -1,12 +1,13 @@
 #!/opt/local/bin/node
  // dump my entire citibike station list to sqlite
+ // bicycle speed depends on terrain http://googleblog.blogspot.com/2010/03/biking-directions-added-to-google-maps.html
 'use strict';
+// home station 521 is west side of penn station, near post office
+var homeStationId = 521;
 var config = require('../lib/configger.js');
 var async = require('async');
 var sqlite3 = require('sqlite3').verbose();
 var LookupStation = require('../lib/lookupStation.js');
-var CitibikeTrips = require('CitibikeTrips');
-var Citibike = require('citibike');
 var distance = require('google-distance');
 var ls = new LookupStation(config);
 
@@ -17,6 +18,7 @@ var db = new sqlite3.Database(config.path.sqldb, function(err) {
 	}
 });
 
+// TODO these functions should move into a library
 var dbSaveTripLength = function dbSaveTripLengthF(err, newRow) {
 	// save a distance record to database
 	if (err) return console.log(err);
@@ -29,11 +31,9 @@ var dbSaveTripLength = function dbSaveTripLengthF(err, newRow) {
 
 var computeTripLength = function computeTripLengthF(homeId, destId, callback) {
 	// get a distance record from database if exists, else lookup from google and save it
-	// console.log("compute from " + homeId + " to " + destId);
 
 	db.get("SELECT * FROM distance WHERE startStationId == ? AND endStationId == ?", homeId, destId, function(err, row) {
-		// if (err) throw err;
-		// console.log("results from " + row);
+		if (err) throw err;
 
 		if (row === undefined) {
 			// no row, go do lookup
@@ -48,8 +48,6 @@ var computeTripLength = function computeTripLengthF(homeId, destId, callback) {
 					}
 				],
 				function(err, homeDestLatLong) {
-					// results is now equals to: {one: 1, two: 2}
-
 					// console.log("Getting distance from " + homeId + " to " + destId + ' latlong ' + homeDestLatLong);
 
 					distance.get({
@@ -60,7 +58,6 @@ var computeTripLength = function computeTripLengthF(homeId, destId, callback) {
 						},
 						function(err, data) {
 							if (err) return console.log(err);
-							// console.log(data);
 
 							// save result before returning it
 							dbSaveTripLength(err, [homeId,
@@ -90,6 +87,7 @@ var computeTripLength = function computeTripLengthF(homeId, destId, callback) {
 			);
 		} else {
 			// we got a cached row
+			console.log('cached');
 			callback(null, row);
 		}
 	});
@@ -101,20 +99,31 @@ db.serialize(function() {
 
 	db.run("CREATE TABLE IF NOT EXISTS distance ( startStationId int, endStationId int, distance text, distanceMeters int, duration text, durationSec int, startStationAddr text, endStationAddr text)");
 	db.run("CREATE UNIQUE INDEX IF NOT EXISTS distanceun ON distance (startStationId, endStationId)");
-	var homeStationId = 512;
 
 	// startStationId , endStationId , distance , distanceMeters , duration , durationSec , startStationAddr , endStationAddr
+
+	// estimate every trip I've recorded
 	db.each("select distinct startStationId, endStationId from trips order by 1, 2", function(err, row) {
 		computeTripLength(row.startStationId, row.endStationId, function(err, row) {
 			console.log(JSON.stringify(row));
 		});
 	});
 
+	// estimate trip from my homeStation to everywhere I've been
 	db.each("select startStationId id from trips union select endStationId id from trips", function(err, row) {
 		computeTripLength(homeStationId, row.id, function(err, row) {
 			console.log(JSON.stringify(row));
 		});
 	});
+
+	// estimate trip from my homeStation to every stations
+	db.each("select id from stations", function(err, row) {
+		computeTripLength(homeStationId, row.id, function(err, row) {
+			console.log(JSON.stringify(row));
+		});
+	});
+
+	// closing too soon, nodejs is always too soon
 	// db.close();
 });
 /* fin */
