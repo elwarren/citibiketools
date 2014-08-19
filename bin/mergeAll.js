95de1dee0b8231785202546b3bc6bc1334b99360
Warren Lindsey <warren.lindsey@gmail.com>
2014-08-19 01:09:58 -0400

    more sql reports

---
+++ bin/mergeAll.js
@@ -0,0 +1,88 @@
+#!/opt/local/bin/node
+ // dump my entire citibike history to sqlite
+'use strict';
+var config = require('../lib/configger');
+var CitibikeTrips = require('CitibikeTrips');
+var Citibike = require('Citibike');
+var citibike = new Citibike;
+var ct = new CitibikeTrips(config);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(config.path.sqldb, function(err) {
+	if (err) {
+		console.warn('Error opening database file [' + config.path.sqldb + ']');
+		throw err;
+	} else {
+		console.log('Opened sqlite3 file [' + config.path.sqldb + ']');
+	}
+});
+
+ct.getAllTrips(function(err) {
+	if (err) {
+		console.warn(err);
+		return;
+	}
+
+	console.log('Got trips');
+
+	if (config.path.sqldb) {
+		citibike.getStations(null, function(data) {
+			console.log('Got stations');
+
+			// id , status,	latitude, longitude, label, stationAddress, availableBikes, availableDocks, nearbyStations;
+			db.serialize(function() {
+				db.run("CREATE TABLE IF NOT EXISTS stations ( id int PRIMARY KEY, status varchar2(20), latitude float, longitude float, label varchar2(60), stationAddress varchar2(60), availableBikes int, availableDocks int, nearbyStations varchar2(2000))");
+				db.run("CREATE TABLE IF NOT EXISTS stationsNearby ( id int, nearbyId, distance float )");
+				db.run("CREATE UNIQUE INDEX IF NOT EXISTS snun ON stationsNearby (id, nearbyId)");
+				db.run("CREATE TABLE IF NOT EXISTS trips (" +
+					"id int PRIMARY KEY, startStationId int, startTimestamp int, " +
+					"endStationId int, endTimestamp int, durationSeconds int, " +
+					"endDate date, startDate date, durationMins int, nowSecs int, " +
+					"nowMins int, isOpen boolean, retrievedTimestamp int )");
+
+				var stmtStations = db.prepare("INSERT OR REPLACE INTO stations VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				var stmtNearby = db.prepare("INSERT OR REPLACE INTO stationsNearby VALUES (?, ?, ?)");
+
+				for (var i = 0, d = data.results.length; i < d; i++) {
+					stmtStations.run(data.results[i].id,
+						data.results[i].status,
+						data.results[i].latitude,
+						data.results[i].longitude,
+						data.results[i].label,
+						data.results[i].stationAddress,
+						data.results[i].availableBikes,
+						data.results[i].availableDocks,
+						data.results[i].nearbyStations);
+
+					// nearbyStations is nested within station
+					for (var c = 0; c < 5; c++) {
+						stmtNearby.run(data.results[i].id,
+							data.results[i].nearbyStations[c].id,
+							data.results[i].nearbyStations[c].distance);
+					}
+				}
+				stmtStations.finalize();
+				stmtNearby.finalize();
+
+				var stmtTrips = db.prepare("INSERT OR REPLACE INTO trips " +
+					"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				for (var trip in ct.trips) {
+					stmtTrips.run(ct.trips[trip].id,
+						ct.trips[trip].startStationId, ct.trips[trip].startTimestamp,
+						ct.trips[trip].endStationId, ct.trips[trip].endTimestamp,
+						ct.trips[trip].durationSeconds, ct.trips[trip].endDate,
+						ct.trips[trip].startDate, ct.trips[trip].durationMins,
+						ct.trips[trip].nowSecs, ct.trips[trip].nowMins, ct.trips[trip].isOpen,
+						ct.trips[trip].retrievedTimestamp);
+				}
+				stmtTrips.finalize();
+
+				db.close();
+			});
+		});
+	} else {
+		console.log(JSON.stringify(ct.trips, 0, 2));
+
+	}
+});
+
+/* fin */
---
+++ sql/trips_per_day_last4weeks_total_weekly_mins.sql
@@ -0,0 +1,136 @@
+-- highlight which days of week you rode
+-- over the last four weeks
+-- with total minutes of weekly ride time
+--
+-- http://www.sqlite.org/lang_datefunc.html
+-- MTWTFSS
+-- 1h 27m
+-- 0h 46m
+-- 1h 56m
+-- Calendar-star-white 0h 10m
+-- %d		day of month: 00
+-- %f		fractional seconds: SS.SSS
+-- %H		hour: 00-24
+-- %j		day of year: 001-366
+-- %J		Julian day number
+-- %m		month: 01-12
+-- %M		minute: 00-59
+-- %s		seconds since 1970-01-01
+-- %S		seconds: 00-59
+-- %w		day of week 0-6 with Sunday==0
+-- %W		week of year: 00-53
+-- %Y		year: 0000-9999
+-- %%		%
+--
+-- woy         mon         tue         wed         thu         fri         sat         sun         minPerWeek
+-- ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------
+-- 30          1           2           2           2           2           6                       36        
+-- 31          2           2           2           2           3                       1           33        
+-- 32          2           1           1           2           1           10                      46        
+-- 33          1                                                                                   5         
+--                                                           
+
+.headers on
+.mode column
+select woy, max(mon) mon, max(tue) tue, max(wed) wed, max(thu) thu, max(fri) fri, max(sat) sat, max(sun) sun, sum(durationSeconds)/60 minPerWeek
+  from (
+select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , count(*) mon
+	 , null tue
+	 , null wed
+	 , null thu
+	 , null fri
+	 , null sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 1
+ group by 1
+ union
+select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , count(*) tue
+	 , null wed
+	 , null thu
+	 , null fri
+	 , null sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 2
+ group by 1
+ union
+ select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , null tue
+	 , count(*) wed
+	 , null thu
+	 , null fri
+	 , null sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 3
+ group by 1
+ union
+ select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , null tue
+	 , null wed
+	 , count(*) thu
+	 , null fri
+	 , null sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 4
+ group by 1
+ union
+ select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , null tue
+	 , null wed
+	 , null thu
+	 , count(*) fri
+	 , null sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 5
+ group by 1
+ union
+ select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , null tue
+	 , null wed
+	 , null thu
+	 , null fri
+	 , count(*) sat
+	 , null sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 6
+ group by 1
+ union
+ select strftime('%W', startTimestamp, 'unixepoch', 'localtime') woy
+	 , null mon
+	 , null tue
+	 , null wed
+	 , null thu
+	 , null fri
+	 , null sat
+	 , count(*) sun
+	 , durationSeconds
+  from trips 
+ where abs(strftime('%Y%W', startTimestamp, 'unixepoch', 'localtime')) > abs(strftime('%Y%W', 'now')) - 4
+   and abs(strftime('%w', startTimestamp, 'unixepoch', 'localtime')) = 0
+ group by 1
+ ) 
+group by 1
+order by 1;
