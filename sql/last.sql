2d00471d02348488fb93a9988a37710014dbb7d2
Warren Lindsey <warren.lindsey@mlb.com>
2014-09-09 18:11:44 -0400

    updated sql to handle currently open trips

--- sql/last.sql
+++ sql/last.sql
@@ -1,5 +1,36 @@
 -- most recent trip, with start and end station names
--- TODO trying to detect open trips with endStationId=='Station Id - null : null' need to test with open trip data
+--           id = 14865853
+-- startStation = 8 Ave & W 31 St
+--   endStation = 9 Ave & W 18 St
+--      startId = 521
+--        endId = 434
+--       isOpen = 0
+--   isOpenCalc = 0
+--         Date = 2014-09-09
+--    startTime = 10:45:13
+--      endTime = 10:49:41
+--      nowTime = 18:05:07
+--  tillNowSecs = 26394
+--  tillNowMins = 439
+-- tillNowHours = 7
+-- 
+-- NOTE: the replace line is to detect open trips with endStationId=='Station Id - null : null'
+--       when cast to int it becomes 0 and there are no zero stations, so declare open trip
+--       if it is normal integer station id then no match and trip is declared closed
+--           id = 14839068
+-- startStation = W 16 St & The High Line
+--   endStation =
+--      startId = 212
+--        endId = Station Id - null : null
+--       isOpen = 1
+--   isOpenCalc = 1
+--         Date = 2014-09-08
+--    startTime = 18:33:40
+--      endTime =
+--          now = 1410279978
+--  tillNowSecs = 64358
+--  tillNowMins = 1072
+-- tillNowHours = 17
 .headers on
 .mode line
 select t.id
@@ -8,12 +39,11 @@ select t.id
      , t.startStationId startId
      , t.endStationId endId
      , t.isOpen isOpen
-     -- , replace(0, cast('Station Id - null : null' as int), 1) isOpenCalc
-     , replace(0, cast(endStationId as int), 1) isOpenCalc
+     , replace(0, cast(t.endStationId as int), 1) isOpenCalc
      , date(startTimestamp, 'unixepoch', 'localtime') Date
      , time(startTimestamp, 'unixepoch', 'localtime') startTime
      , time(endTimestamp, 'unixepoch', 'localtime') endTime
-     , cast(strftime("%s", CURRENT_TIMESTAMP) as int) now
+     , time('now', 'localtime') nowTime
      , cast(strftime("%s", CURRENT_TIMESTAMP) as int) - startTimestamp tillNowSecs
      , cast((cast(strftime("%s", CURRENT_TIMESTAMP) as int) - startTimestamp) / 60 as int) tillNowMins
      , cast((cast(strftime("%s", CURRENT_TIMESTAMP) as int) - startTimestamp) / 60 / 60 as int) tillNowHours
--- sql/today.sql
+++ sql/today.sql
@@ -1,8 +1,13 @@
--- BAM-028869-WL:CitiBike lindsey$ sqlite3 data/citibike.sqlite3 < citibiketools/sql/today.sql
 -- Date        TimeBeg     TimeEnd     Secs        distance    IdBeg       IdEnd       StationBeg       StationEnd       FASTEST     AVERAGE     SLOWEST     Trips
 -- ----------  ----------  ----------  ----------  ----------  ----------  ----------  ---------------  ---------------  ----------  ----------  ----------  ----------
 -- 2014-09-04  10:27:42    10:32:57    315         0.8 mi      521         434         8 Ave & W 31 St  9 Ave & W 18 St  279         321         375         14
 -- 2014-09-04  19:16:10    19:23:07    417         1.1 mi      212         512         W 16 St & The H  W 29 St & 9 Ave  267         473         1107        17
+--
+-- Example of an open trip
+-- Date        TimeBeg     TimeEnd     Secs        distance    IdBeg       IdEnd       StationBeg       StationEnd       FASTEST     AVERAGE     SLOWEST     Trips
+-- ----------  ----------  ----------  ----------  ----------  ----------  ----------  ---------------  ---------------  ----------  ----------  ----------  ----------
+-- 2014-09-08  10:40:40    10:47:31    411         1.0 mi      379         434         W 31 St & 7 Ave  9 Ave & W 18 St  323         367         411         2
+-- 2014-09-08  18:33:40                0                       212         Station Id  W 16 St & The H                   0           0           0           1
 .headers on
 .mode column
 select date(t.startTimestamp, 'unixepoch', 'localtime') Date
@@ -11,7 +16,7 @@ select date(t.startTimestamp, 'unixepoch', 'localtime') Date
      , t.durationSeconds Secs
      , d.distance
      , t.startStationId IdBeg
-     , t.endStationId IdEnd
+     , cast(t.endStationId as int) IdEnd
      , sa.label StationBeg
      , sb.label StationEnd
      , tmm.fastest
@@ -21,15 +26,13 @@ select date(t.startTimestamp, 'unixepoch', 'localtime') Date
   from trips t left join distance d on t.startStationId = d.startStationId and t.endStationId = d.endStationId
                left join stations sa on t.startStationId = sa.id 
                left join stations sb on t.endStationId = sb.id
-     , ( select fas.startStationId, fas.endStationId
-              , min(fas.durationSeconds) FASTEST
-              , cast(avg(fas.durationSeconds) as int) AVERAGE
-              , max(fas.durationSeconds) SLOWEST 
-              , count(*) TOTAL
-           from trips fas
-          group by fas.startStationId, fas.endStationId 
-       ) tmm
- where t.startStationId = tmm.startStationId 
-   and t.endStationId = tmm.endStationId 
-   and date(t.startTimestamp, 'unixepoch', 'localtime') = date('now', 'localtime')
+               left join ( select fas.startStationId, fas.endStationId
+                                , min(fas.durationSeconds) FASTEST
+                                , cast(avg(fas.durationSeconds) as int) AVERAGE
+                                , max(fas.durationSeconds) SLOWEST 
+                                , count(*) TOTAL
+                             from trips fas
+                            group by fas.startStationId, fas.endStationId 
+                         ) tmm on t.startStationId = tmm.startStationId and t.endStationId = tmm.endStationId 
+ where date(t.startTimestamp, 'unixepoch', 'localtime') = date('now', 'localtime')
  order by 1 asc, 2 asc;
